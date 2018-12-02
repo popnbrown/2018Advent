7121ffda948968a46bdf444e9ad84d354512467a
Sravan Suryadevara <ssuryadevara@firstinspires.org>
2018-12-02 17:17:46 -0500

    initial commit

---
+++ src/Day1_Part1.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day1_Part1 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		File file = new File("test.txt"); 
+		  
+		BufferedReader br = new BufferedReader(new FileReader(file)); 
+		  
+		String st;
+		int sum = 0;
+		while ((st = br.readLine()) != null) 
+			sum += Integer.parseInt(st);
+		System.out.println(sum);
+	}
+
+}
